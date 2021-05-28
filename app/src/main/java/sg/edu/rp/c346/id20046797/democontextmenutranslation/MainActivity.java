0408489f7930b6950f496bf46c63203360d10e9f
TheSchoolFriday <20046797@myrp.edu.sg>
2021-05-28 09:49:27 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20046797/democontextmenutranslation/MainActivity.java
@@ -0,0 +1,24 @@
+package sg.edu.rp.c346.id20046797.democontextmenutranslation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvTranslatedText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvTranslatedText = findViewById(R.id.textViewTranslatedText);
+
+        registerForContextMenu(tvTranslatedText);
+    }
+
+    
+}
